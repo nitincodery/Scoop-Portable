139765aa9905d7c6d955e3fa9c86b90d378150fd
nitincodery <54881266+nitincodery@users.noreply.github.com>
2025-05-23 17:29:37 +0530

    FIX: Disable shortcuts creation

--- lib/install.ps1
+++ lib/install.ps1
@@ -57,7 +57,7 @@ function install_app($app, $architecture, $global, $suggested, $use_cache = $tru
     ensure_install_dir_not_in_path $dir $global
     $dir = link_current $dir
     create_shims $manifest $dir $global $architecture
-    create_startmenu_shortcuts $manifest $dir $global $architecture
+    # create_startmenu_shortcuts $manifest $dir $global $architecture
     install_psmodule $manifest $dir $global
     env_add_path $manifest $dir $global $architecture
     env_set $manifest $global $architecture
